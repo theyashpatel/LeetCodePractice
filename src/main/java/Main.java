e1f44399decc66602c42132450edec42b1b2dba4
Yash <yashpatel26@gmail.com>
2021-09-09 11:04:36 -0500

    merge sorted array

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,13 @@
+import easy.MergeSortedArray;
+
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args) {
+    int[] num1 = new int[]{1, 2, 3, 0, 0, 0};
+    int[] num2 = new int[]{2, 5, 6};
+    MergeSortedArray ms = new MergeSortedArray();
+    ms.merge(num1, num1.length - num2.length, num2, num2.length);
+    System.out.println(Arrays.toString(num1));
   }
 }
---
+++ src/test/java/easy/MergeSortedArrayTest.java
@@ -0,0 +1,5 @@
+package easy;
+
+public class MergeSortedArrayTest {
+// figuring out how to test this one
+}
